f0dbc174ad5f7f9adc37d1e52c2b04a1d9731d10
Lukis <lukas.smyczek@tuta.io>
2025-10-07 13:29:37 +0200

    books can now be added to the database

--- routes/book.js
+++ routes/book.js
@@ -1,13 +1,46 @@
 var express = require('express');
+const sqlite3 = require("sqlite3");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('media-list', { title: 'Books', route: 'book' });
+    const db = new sqlite3.Database('backlog.db');
+
+    var query = "SELECT name FROM book";
+    db.all(query, function (err, rows) {
+        if(err){
+            console.log(err);
+        }else{
+            res.render('media-list', { title: 'Books', route: 'book', list: rows});
+        }
+    });
 });
 
 router.get('/add', function(req, res, next) {
     res.render('media-form', { title: 'Books' });
 });
+
+router.post('/add', function(req, res, next) {
+    const db = new sqlite3.Database('backlog.db');
+
+    var name = req.body.name;
+    var year = Number(req.body.year);
+    var genre = req.body.genre;
+    var country = req.body.country;
+    var description = req.body.description;
+    var date_added = new Date();
+    var status = 'open';
+
+    var author = req.body.author;
+    var length = req.body.length;
+    var publisher = req.body.publisher;
+    var illustrator = req.body.illustrator;
+
+    const sql = "INSERT INTO book (name, year, genre, country, description, status, added, author, length, publisher, illustrator)" +
+        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    db.run(sql, [name, year, genre, country, description, status, date_added, author, length, publisher, illustrator]);
+
+    res.redirect('/book')
+})
 
 module.exports = router;
