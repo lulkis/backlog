7d150758f1d140da00973456db8512de3462cf5a
Lukis <lukas.smyczek@tuta.io>
2025-10-07 11:31:18 +0200

    setup the media-form with ids and get routing for movies

--- routes/movie.js
+++ routes/movie.js
@@ -10,4 +10,18 @@ router.get('/add', function(req, res, next) {
     res.render('media-form', { title: 'Movies' });
 });
 
+router.post('/add', function(req, res) {
+    console.log(req.body);
+
+    var name = req.body.name;
+    var year = Number(req.body.year);
+    var genre = req.body.genre;
+    var country = req.body.country;
+    var description = req.body.description;
+    var date_added = new Date();
+    var status = 'open';
+
+    res.redirect('/media/books')
+})
+
 module.exports = router;
