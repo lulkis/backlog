d36017aa4d504136c89eaa21c4284aa76cdf6153
Lukis <lukas.smyczek@tuta.io>
2025-10-07 12:04:10 +0200

    making a table for movies in startup, if it not exists

--- routes/database.js
+++ routes/database.js
@@ -6,8 +6,26 @@ export function setup_database() {
         if (err) {
             return console.error(err.message);
         }
-        console.log('Connected to the todo app database.');
+        console.log('Connected to the backlog database.');
     });
+
+    db.run(
+        "CREATE TABLE IF NOT EXISTS movie (" +
+        "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+        "name VARCHAR(128) NOT NULL," +
+        "year INTEGER," +
+        "genre VARCHAR(128)," +
+        "country VARCHAR(128)," +
+        "description TEXT," +
+        "status VARCHAR(128)," +
+        "added VARCHAR(128)," +
+        "studio VARCHAR(128)," +
+        "director VARCHAR(128)," +
+        "length INTEGER," +
+        "cast TEXT," +
+        "CHECK (status IN ('open', 'started', 'finished'))" +
+        ");"
+    );
 
     db.close()
 }
