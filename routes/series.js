1b0b4651190cd5ae745191635107aee3a2bdd5f5
Lukis <lukas.smyczek@tuta.io>
2025-10-07 13:18:36 +0200

    all series will now be listet in the media-list page

--- routes/series.js
+++ routes/series.js
@@ -1,13 +1,47 @@
 var express = require('express');
 var router = express.Router();
+var sqlite3 = require('sqlite3');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('media-list', { title: 'Series', route: 'series' });
+    const db = new sqlite3.Database('backlog.db');
+
+    var query = "SELECT name FROM series";
+    db.all(query, function (err, rows) {
+        if(err){
+            console.log(err);
+        }else{
+            res.render('media-list', { title: 'Series', route: 'series', list: rows});
+        }
+    });
 });
 
 router.get('/add', function(req, res, next) {
     res.render('media-form', { title: 'Series' });
 });
+
+router.post('/add', function(req, res, next) {
+    const db = new sqlite3.Database('backlog.db');
+
+    console.log(req.body);
+
+    var name = req.body.name;
+    var year = Number(req.body.year);
+    var genre = req.body.genre;
+    var country = req.body.country;
+    var description = req.body.description;
+    var date_added = new Date();
+    var status = 'open';
+    var idea = req.body.idea;
+    var studio = req.body.studio;
+    var cast = req.body.cast;
+    var episodes = req.body.episodes;
+
+    const sql = "INSERT INTO series (name, year, genre, country, description, status, added, idea, studio, cast, episodes)" +
+        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    db.run(sql, [name, year, genre, country, description, status, date_added, idea, studio, cast, episodes]);
+
+    res.redirect('/series')
+})
 
 module.exports = router;
