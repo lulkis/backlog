ceab8bce6485e6d6ae6adbb47bc9c1048aecf1f0
Lukis <lukas.smyczek@tuta.io>
2025-10-07 08:26:51 +0200

    adding books to the routing

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var moviesRouter = require('./routes/movie');
 var gamesRouter = require('./routes/game');
+var booksRouter = require('./routes/book');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/movie', moviesRouter);
 app.use('/game', gamesRouter);
+app.use('/book', booksRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
